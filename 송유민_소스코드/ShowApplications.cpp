a2f0dad7cf03c0f6da208013729019b9e018a0f9
yumin9810 <bgso9810@gmail.com>
2023-05-22 04:13:13 +0900

    v1

---
+++ 송유민_소스코드/ShowApplications.cpp
@@ -0,0 +1,23 @@
+#include "ShowApplications.h"
+
+/*
+	함수 이름 : ShowApplicationsUI::showAllApplications()
+	기능	  : 
+	전달 인자 : 없음
+	반환값    : 없음
+*/
+void ShowApplicationsUI::showAllApplications()
+{
+	
+}
+
+/*
+	함수 이름 : ShowApplications::showApplications()
+	기능	  :
+	전달 인자 : 없음
+	반환값    : 없음
+*/
+void ShowApplications::showApplications()
+{
+
+}
---
+++ 송유민_소스코드/main.cpp
@@ -0,0 +1,11 @@
+#include "CancelApplication.h"
+#include "Entities.h"
+#include "ShowApplications.h"
+#include "ShowStatistics.h"
+
+int main()
+{
+	
+
+	return 0;
+}
