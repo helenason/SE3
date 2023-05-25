c0656706e45b82ca8bdfc35eca2ae5e431c79c64
rlagksk0315 <118969189+rlagksk0315@users.noreply.github.com>
2023-05-26 03:30:38 +0900

    final

--- total_code/RegisterRecruitmentUI.h
+++ total_code/RegisterRecruitmentUI.h
@@ -2,6 +2,10 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "Member.h"
 
+/*
+	클래스 이름: RegisterRecruitmentUI
+	클래스 기능: 입력 파일에 쓰여진 값들을 받아 채용 정보로 입력함
+*/
 class RegisterRecruitmentUI {
 public:
 	void inputDetails(FILE* in_fp, FILE* out_fp, Member* loginMember);
