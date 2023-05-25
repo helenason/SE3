1d19ea42305ca7071f9397459d9b7aa9baf9b740
rlagksk0315 <118969189+rlagksk0315@users.noreply.github.com>
2023-05-26 03:36:36 +0900

    final

--- total_code/RegisterRecruitment.cpp
+++ total_code/RegisterRecruitment.cpp
@@ -9,7 +9,8 @@ using namespace std;
 
 /*
     함수 이름: RegisterRecruitment::registerNewRecruitment()
-    기능: 새로운 recruitment instance(newRecruitment)를 생성함; 그러기 위해
+    기능: 새로운 recruitment instance(newRecruitment)를 생성함;
+         그러기 위해 member의 사업자번호, 아이디, 회사번호 저장함
     전달 인자: string task, int numPeople, string applyDeadline, Member* member
     반환값: 생성된 newRecruitment
 */
