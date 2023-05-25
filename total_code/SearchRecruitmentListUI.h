3d0dc366af52ec8360b68730df362cf22f1033ac
rlagksk0315 <118969189+rlagksk0315@users.noreply.github.com>
2023-05-26 03:21:32 +0900

    final

--- total_code/SearchRecruitmentListUI.h
+++ total_code/SearchRecruitmentListUI.h
@@ -4,7 +4,7 @@
 /*
 	클래스 이름: SearchRecruitmentListUI
 	클래스 기능: searchRecruitment() operation으로 화사 직원이 등록한 채용 리스트 출력함
-	*/
+*/
 class SearchRecruitmentListUI {
 public:
 	void searchRecruitment(FILE* out_fp, Member* loginMember);
